028ee4ab6d9d3b5373fc758ea00114fcff3c65a1
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-02-07 18:42:53 +0900

    日数計算表示。

--- src/himekuri/Himekuri.java
+++ src/himekuri/Himekuri.java
@@ -5,9 +5,14 @@
  */
 package himekuri;
 
+import java.io.PrintStream;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  *
@@ -27,12 +32,17 @@ public class Himekuri {
         LocalDateTime d = LocalDateTime.now();
         
         Calendar cal = Calendar.getInstance();
+        
+        LocalDate date1 = LocalDate.of(d.getYear(), d.getMonth(), d.getDayOfMonth());
+        LocalDate date2 = LocalDate.of(2021, 1, 1);
+        
+        long datediff = ChronoUnit.DAYS.between(date1,date2);
         String reiwa = "令和";
         String r = "R";
         
-        
         System.out.println(sdf.format(cal.getTime()));
-        System.out.println(reiwa + (d.getYear()-2018) + sdf2.format(cal.getTime()) + " : " + (r + (d.getYear()-2018) +sdf3.format(cal.getTime())));
+        System.out.println("来年の1月1日まであと：" + datediff + "日です。");
+        System.out.println(reiwa + (d.getYear()-2018) + sdf2.format(cal.getTime()) + ":" + (r + (d.getYear()-2018) +sdf3.format(cal.getTime())));
     }
     
 }
