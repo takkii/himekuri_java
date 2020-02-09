952bc1660f3bbea267598b5cc626a9fa0c6d523c
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-02-09 13:03:37 +0900

    Update.

--- src/himekuri/HimekuriJava.java
+++ src/himekuri/HimekuriJava.java
@@ -15,29 +15,29 @@ class Juke{
         SimpleDateFormat sdf_koyomi = new SimpleDateFormat("yyyy年MM月dd日mm分ss秒");
         SimpleDateFormat sdf_nengo = new SimpleDateFormat("年MM月dd日");
         SimpleDateFormat sdf_kigo = new SimpleDateFormat(".MM.dd");
-        
+
         LocalDateTime d = LocalDateTime.now();
         Calendar cal = Calendar.getInstance();
-        
+
         LocalDate date_before = LocalDate.of(d.getYear(), d.getMonth(), d.getDayOfMonth());
         LocalDate date_after = LocalDate.of(d.getYear() +1, 1, 1);
-        
+
         long datediff = ChronoUnit.DAYS.between(date_before, date_after);
         String reiwa_kanji = "令和";
         String reiwa_alpha = "R";
-        
+
         System.out.println(sdf_koyomi.format(cal.getTime()));
         System.out.println("来年の1月1日まであと：" + (datediff -1) + "日です。");
         System.out.println(reiwa_kanji + (d.getYear()-2018) + sdf_nengo.format(cal.getTime()) + ":" + (reiwa_alpha + (d.getYear()-2018) + sdf_kigo.format(cal.getTime())));
-        
+
         }catch(Exception e){
             System.err.println("Exceptionエラーを捕捉しました...");
             e.printStackTrace(System.out);
-            
+
         }finally{
             String version = "1.0.0";
             String himekuriVersion = "日めくりの数え番号：" + version;
-            
+
             System.out.println(himekuriVersion);
         }
     }
@@ -49,7 +49,7 @@ class Juke{
  */
 public class HimekuriJava {
     public static void main(String[] args) {
-        
+
         // SubClass call
         Juke obj = new Juke();
         obj.himekuri();
