56e4cca86dec38c5a73a19a72d4b6c796813bcdf
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-07-06 19:09:43 +0900

    Update, format.

--- src/himekuri/HimekuriJava.java
+++ src/himekuri/HimekuriJava.java
@@ -11,49 +11,47 @@ import java.util.Calendar;
 
 // Ruco is SubClass.
 class Ruco extends Thread {
+
+    final SimpleDateFormat sdf_koyomi = new SimpleDateFormat("yyyy年MM月dd日HH時mm分ss秒：EEEE");
+    final Calendar cal = Calendar.getInstance();
+
     public void himekuri() {
         try {
             final Ruco thread = new Ruco();
             thread.setDaemon(true);
             thread.start();
 
-            final SimpleDateFormat sdf_koyomi = new SimpleDateFormat("yyyy年MM月dd日HH時mm分ss秒：EEEE");
-            final Calendar cal = Calendar.getInstance();
             System.out.println(sdf_koyomi.format(cal.getTime()));
+
+            final SimpleDateFormat sdf_koyomi = null;
+            final Calendar cal = null;
             thread.join();
 
         } catch (final Exception e) {
             System.err.println("Exceptionエラーを捕捉しました...");
             e.printStackTrace(System.out);
-        } finally {
-            final SimpleDateFormat sdf_koyomi = null;
-            final Calendar cal = null;
         }
     }
 }
 
 class Counter extends Thread {
+    final LocalDateTime d = LocalDateTime.now();
+    final SimpleDateFormat sdf_nengo = new SimpleDateFormat("年MM月dd日");
+    final SimpleDateFormat sdf_kigo = new SimpleDateFormat(".MM.dd");
+    final Calendar cal = Calendar.getInstance();
+    final String reiwa_kanji = "令和";
+    final String reiwa_alpha = "R";
+    final String reiwa_beta = (reiwa_kanji + (d.getYear() - 2018)) + (sdf_nengo.format(cal.getTime()));
+    final String reiwa_delta = (reiwa_alpha + (d.getYear() - 2018)) + (sdf_kigo.format(cal.getTime()));
+
     public void Reiwa_Counter() {
         try {
             final Counter thread = new Counter();
-            thread.start();
 
-            final LocalDateTime d = LocalDateTime.now();
-            final SimpleDateFormat sdf_nengo = new SimpleDateFormat("年MM月dd日");
-            final SimpleDateFormat sdf_kigo = new SimpleDateFormat(".MM.dd");
-            final Calendar cal = Calendar.getInstance();
-            final String reiwa_kanji = "令和";
-            final String reiwa_alpha = "R";
-            final String reiwa_beta = (reiwa_kanji + (d.getYear() - 2018)) + (sdf_nengo.format(cal.getTime()));
-            final String reiwa_delta = (reiwa_alpha + (d.getYear() - 2018)) + (sdf_kigo.format(cal.getTime()));
+            thread.start();
 
             System.out.println(reiwa_beta + "：" + reiwa_delta);
 
-            thread.join();
-        } catch (final Exception ee) {
-            System.err.println("Exceptionエラーを捕捉しました...");
-            ee.printStackTrace(System.out);
-        } finally {
             final LocalDateTime d = null;
             final SimpleDateFormat sdf_nengo = null;
             final SimpleDateFormat sdf_kigo = null;
@@ -62,32 +60,39 @@ class Counter extends Thread {
             final String reiwa_alpha = null;
             final String reiwa_beta = null;
             final String reiwa_delta = null;
+
+            thread.join();
+
+        } catch (final Exception ee) {
+            System.err.println("Exceptionエラーを捕捉しました...");
+            ee.printStackTrace(System.out);
         }
     }
 }
 
 class Hizuke extends Thread {
+    final LocalDateTime d = LocalDateTime.now();
+    final LocalDate date_before = LocalDate.of(d.getYear(), d.getMonth(), d.getDayOfMonth());
+    final LocalDate date_after = LocalDate.of(d.getYear() + 1, 1, 1);
+    final long datediff = ChronoUnit.DAYS.between(date_before, date_after);
+
     public void Hizuke_counter() {
         try {
             final Hizuke thread = new Hizuke();
             thread.start();
 
-            final LocalDateTime d = LocalDateTime.now();
-            final LocalDate date_before = LocalDate.of(d.getYear(), d.getMonth(), d.getDayOfMonth());
-            final LocalDate date_after = LocalDate.of(d.getYear() + 1, 1, 1);
-            final long datediff = ChronoUnit.DAYS.between(date_before, date_after);
-
             System.out.println("来年の1月1日まであと：" + (datediff - 1) + "日です。");
+
+            final LocalDateTime d = null;
+            final LocalDate date_before = null;
+            final LocalDate date_after = null;
+            final long datediff = 0L;
+
 
             thread.join();
         } catch (final Exception eee) {
             System.err.println("Exceptionエラーを捕捉しました...");
             eee.printStackTrace(System.out);
-        } finally {
-            final LocalDateTime d = null;
-            final LocalDate date_before = null;
-            final LocalDate date_after = null;
-            final long datediff = 0L;
         }
     }
 }
@@ -95,22 +100,23 @@ class Hizuke extends Thread {
 
 // Seel is SubClass
 class Seel extends Thread {
+    final String version = "1.0.2";
+    final String himekuriVersion = "日めくりの数え番号：" + version;
+
     public void himekuri() {
         try {
             final Seel thread = new Seel();
             thread.start();
 
-            final String version = "1.0.2";
-            final String himekuriVersion = "日めくりの数え番号：" + version;
             System.out.println(himekuriVersion);
+            final String version = null;
+            final String himekuriVersion = null;
+
 
             thread.join();
         } catch (final Exception ex) {
             System.err.println("Exceptionエラーを捕捉しました...");
             ex.printStackTrace(System.out);
-        } finally {
-            final String version = null;
-            final String himekuriVersion = null;
         }
     }
 }
@@ -118,7 +124,7 @@ class Seel extends Thread {
 /**
  * @author takkii(Takayuki Kamiyama) App is SuperClass. mvn build.
  */
-public class HimekuriJava {
+public class App {
     public static void main(final String[] args) {
 
         // SubClass call
